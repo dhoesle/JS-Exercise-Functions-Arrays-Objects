161b97c0b92551d256c6ee0b4c4452eeebf15a93
Daniel Hoesle <d1@PacBook-Mro.local>
2020-05-14 13:36:21 -0600

    completed challenge sayGoodbye

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return('Goodbye, '+name+' have a great day.')
 }
-
+console.log(sayGoodbye('Danny'));
 /**
  * ### Challenge `temperatureCtoF`
  * 
